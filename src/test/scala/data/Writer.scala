ad27c097251f310a017e0d4163442cc6f82b62b8
Tomoki Mizogami <tomoki.mizogami@nextbeat.net>
2020-11-20 18:53:59 +0900

    Add test for Writer

---
+++ src/test/scala/data/Writer.scala
@@ -0,0 +1,34 @@
+package category.data
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import category.Implicits._
+import category.data.Writer
+import category.data.Writer._
+
+class WriterSpec extends AnyFlatSpec with Matchers {
+
+  def increment(n: Int): Writer[String, Int] =
+    Writer(("increment ", n + 1))
+
+  def isEven(n: Int): Writer[String, Boolean] =
+    Writer(("isEven ", n % 2 == 0))
+
+  def negate(b: Boolean): Writer[String, Boolean] =
+    Writer(("negate ", !b))
+
+  "Writer" should "結合律を満たす" in {
+    assert(
+      ((increment _ >=> isEven) >=> negate)(3) ==
+      (increment _ >=> (isEven _ >=> negate))(3)
+    )
+  }
+
+  "Writer" should "単位律を満たす" in {
+    assert(
+      (Writer.pure[String, Int] _ >=> isEven)(3) ==
+      (isEven _ >=> Writer.pure[String, Boolean])(3)
+    )
+  }
+}
