961dfdfeaf550403a17bc882815f514e62c8b343
taretmch <tomochuri25@gmail.com>
2021-03-22 01:26:14 +0900

    Refactor Bifunctor instance

--- core/src/main/scala/hamcat/data/instance/Bifunctor.scala
+++ core/src/main/scala/hamcat/data/instance/Bifunctor.scala
@@ -6,14 +6,14 @@ import hamcat.data.Bifunctor
 trait BifunctorInstances {
 
   /** Product bifunctor */
-  implicit val tuple2Bifunctor = new Bifunctor[Tuple2] {
+  implicit def tuple2Bifunctor = new Bifunctor[Tuple2] {
     def bimap[A, B, C, D](f: A => C)(g: B => D): ((A, B)) => ((C, D)) = {
       case (a, b) => (f(a), g(b))
     }
   }
 
   /** Coproduct bifunctor */
-  implicit val eitherBifunctor = new Bifunctor[Either] {
+  implicit def eitherBifunctor = new Bifunctor[Either] {
     def bimap[A, B, C, D](f: A => C)(g: B => D): Either[A, B] => Either[C, D] = {
       case Left(a)  => Left(f(a))
       case Right(b) => Right(g(b))
