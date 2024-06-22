6387caa1bbbd6b24b255f95c1f300aae694ebea7
taretmch <tomoki.mizogami@gmail.com>
2024-06-22 13:53:16 +0900

    fix(data): fix warning
    warning: "The syntax `<function> _` is no longer supported; you can simply leave out the trailing ` _`. This construct can be rewritten automatically under -rewrite -source 3.4-migration."

--- core/src/main/scala/hamcat/data/Writer.scala
+++ core/src/main/scala/hamcat/data/Writer.scala
@@ -6,10 +6,10 @@ import hamcat.syntax.SemigroupSyntax
 case class Writer[L, A](run: (L, A)):
 
   def flatMap[B](m2: A => Writer[L, B])(using Semigroup[L]): Writer[L, B] =
-    (identity[Writer[L, A]] _ >=> m2)(this)
+    ((identity[Writer[L, A]]) >=> m2)(this)
 
   def fmap[B](f: A => B)(using Monoid[L]): Writer[L, B] =
-    (identity[Writer[L, A]] _ >=> (a => Writer.pure[L, B](f(a))))(this)
+    (identity[Writer[L, A]] >=> (a => Writer.pure[L, B](f(a))))(this)
 
 object Writer extends SemigroupSyntax:
 
