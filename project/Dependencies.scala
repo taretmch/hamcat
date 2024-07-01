86077fd1b7e5bc25012ab4864e562ac939000aff
taretmch <tomoki.mizogami@gmail.com>
2024-07-01 14:01:07 +0900

    deps(core): add munit to dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,5 @@ object Dependencies {
 
   val scalaVersion = "3.4.2"
 
-  val scalatest = Seq(
-    "org.scalactic" %% "scalactic" % "3.2.9",
-    "org.scalatest" %% "scalatest" % "3.2.9" % "test"
-  )
+  val munit = "org.scalameta" %% "munit" % "1.0.0" % Test
 }
