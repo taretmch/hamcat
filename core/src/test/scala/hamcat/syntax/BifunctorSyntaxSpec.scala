dd0c0e21bce5cdb8ce03ecb29bf913a04e0c01a9
taretmch <tomoki.mizogami@gmail.com>
2024-07-02 23:59:02 +0900

    test(instance): add tests for syntax
    todo: add tests for profunctor syntax. profunctor syntax has bugs and is not working properly.

---
+++ core/src/test/scala/hamcat/syntax/BifunctorSyntaxSpec.scala
@@ -0,0 +1,20 @@
+package hamcat.syntax
+
+import hamcat.instance.tuple.given
+import hamcat.instance.either.given
+
+import hamcat.syntax.bifunctor.*
+
+class BifunctorSyntaxSpec extends munit.FunSuite:
+
+  test("bimap : tuple") {
+    val tuple = (1, "hello")
+    val expected = ("1", 5)
+    assertEquals(tuple.bimap(_.toString, _.length), expected)
+  }
+
+  test("bimap : either") {
+    val either: Either[Nothing, String] = Right("hello")
+    val expected = Right(5)
+    assertEquals(either.bimap(identity, _.length), expected)
+  }
---
+++ core/src/test/scala/hamcat/syntax/FunctorSyntaxSpec.scala
@@ -0,0 +1,20 @@
+package hamcat.syntax
+
+import hamcat.instance.list.given
+import hamcat.instance.option.given
+
+import hamcat.syntax.functor.*
+
+class FunctorSyntaxSpec extends munit.FunSuite:
+
+  test("fmap : List") {
+    val list = List(1, 2, 3)
+    val expected = List(2, 3, 4)
+    assertEquals(list.fmap(_ + 1), expected)
+  }
+
+  test("fmap : Option") {
+    val option = Option(42)
+    val expected = Option(43)
+    assertEquals(option.fmap(_ + 1), expected)
+  }
